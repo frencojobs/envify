66d1ebb46b9d2adf00a5e8244dc805891e502493
Frenco <hey@frenco.dev>
2021-08-03 21:39:17 +0630

    refactor: fix linter errors

--- envify_generator/lib/builder.dart
+++ envify_generator/lib/builder.dart
@@ -1,8 +1,8 @@
 library envify_generator.builder;
 
 import 'package:build/build.dart';
-import 'package:source_gen/source_gen.dart';
 import 'package:envify_generator/envify_generator.dart';
+import 'package:source_gen/source_gen.dart';
 
 /// Primary builder to build the generated code from the `EnvifyGenerator`
 Builder envifyBuilder(BuilderOptions options) =>
--- envify_generator/test/envify_generator_test.dart
+++ envify_generator/test/envify_generator_test.dart
@@ -1,9 +1,9 @@
 @TestOn('vm')
-
-import 'package:test/test.dart';
-import 'package:path/path.dart' as path;
 import 'package:envify_generator/envify_generator.dart';
+import 'package:path/path.dart' as path;
 import 'package:source_gen_test/source_gen_test.dart';
+import 'package:test/test.dart';
+
 import 'src/helper_tests.dart' as helpers;
 
 Future<void> main() async {
